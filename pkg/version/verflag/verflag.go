7456525989749f3fc78d5eac6f3c0bc013eca46d
Yongjun <wangyj641@gmail.com>
2024-07-17 10:06:22 +0800

    A simple REST server

--- pkg/version/verflag/verflag.go
+++ pkg/version/verflag/verflag.go
@@ -1,10 +1,8 @@
-// Copyright 2022 Innkeeper Belm(孔令飞) <nosbelm@qq.com>. All rights reserved.
+// Copyright 2024 Yongjun Wang <wangyj641@gmail.com>. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file. The original repo for
-// this file is https://github.com/marmotedu/miniblog.
+// this file is https://github.com/wangyj641/miniblog.
 
-// Package verflag defines utility functions to handle command line flags
-// related to version of IAM.
 package verflag
 
 import (
